dd4a3f90d666398f4d927e6bd730bd9dfa73c2e2
Ian Salt <ian@isalt.digital>
2018-06-20 13:10:07 +0100

    first commit and scaffolding

---
+++ src/MainApp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, Text, TouchableHighlight }
+  from 'react-native';
+
+//  to keep the state of an object we create a class ClassName {
+  class MainApp extends Component {
+    state = {
+      liked: false,
+    }
+
+    _onPressButton = () => {
+      //  we will define the contents in step C
+    }
+
+    //  define the content of the component inside render
+    render() {
+      return (
+        
+      );
+    }
+  }
